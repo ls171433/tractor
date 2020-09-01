fb12dc1bb0f9ca3c26a4ddbd651adb4da8809cb2
ls171433 <lslslsls27@hotmail.com>
2020-09-01 13:51:33 +0800

    move definition to hpp

--- include/exception/exception.hpp
+++ include/exception/exception.hpp
@@ -6,30 +6,63 @@
 #include <exception>
 // for std::string
 #include <string>
+// for std::move
+#include <utility>
 
 
 
+// class text_exception;
 namespace tractor
 {
     class text_exception : public std::exception
     {
     public:
         text_exception();
-        text_exception(const text_exception& other);
-        text_exception(text_exception&& other);
+        text_exception(const text_exception&) = default;
+        text_exception(text_exception&&) = default;
         text_exception(const std::string& text);
+        text_exception(std::string&& text);
         text_exception(const std::exception& e);
 
-        virtual ~text_exception() noexcept;
+        virtual ~text_exception() noexcept override = default;
 
-        text_exception& operator=(const text_exception&);
-        text_exception& operator=(text_exception&&);
+        text_exception& operator=(const text_exception&) = default;
+        text_exception& operator=(text_exception&&) = default;
 
-        virtual const char* what() noexcept;
+        virtual const char* what() const noexcept override;
 
     protected:
         std::string m_text;
     };
+
+
+
+    inline text_exception::text_exception() :
+        text_exception{"tractor::text_exception"}
+    {
+    }
+
+    inline text_exception::text_exception(const std::string& text) :
+        m_text{text}
+    {
+    }
+
+    inline text_exception::text_exception(std::string&& text) :
+        m_text{std::move(text)}
+    {
+    }
+
+    inline text_exception::text_exception(const std::exception& e) :
+        text_exception{e.what()}
+    {
+    }
+
+
+
+    inline const char* text_exception::what() const noexcept
+    {
+        return m_text.c_str();
+    }
 }
 
 
