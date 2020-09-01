bb280eb63defc480aaaa29c86333db25212a93f5
ls171433 <lslslsls27@hotmail.com>
2020-09-01 16:11:31 +0800

    add test

---
+++ test/test_main.cpp
@@ -0,0 +1,30 @@
+#include "test.hpp"
+
+#include <exception>
+
+#include <cstdio>
+
+
+
+test_module_ptr test_module::s_test_module_list[test_module_max];
+int test_module::s_test_module_count = 0;
+
+
+
+int main()
+{
+    for (test_module_ptr current_test_module_ptr : test_module::s_test_module_list)
+    {
+        if (current_test_module_ptr)
+        {
+            try
+            {
+                current_test_module_ptr->test();
+            }
+            catch (std::exception& e)
+            {
+                std::printf("%s", e.what());
+            }
+        }
+    }
+}
