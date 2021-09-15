c4c20ee5e19ee8e732e502712f6dc70e7f658e21
ls171433 <lslslsls27@hotmail.com>
2021-09-15 11:28:05 +0800

    use gtest for exception

--- test/test_exception.cpp
+++ test/test_exception.cpp
@@ -1,171 +1,266 @@
 #include "exception/exception.hpp"
 
-#include "test.hpp"
+#include <gtest/gtest.h>
 
-#include <iostream>
+#include <exception>
+#include <string>
+#include <utility>
 
-using namespace std;
 using namespace tractor;
 
-struct test_exception : test_module
+// test_text_exception
+namespace
 {
-    virtual void test() override
+    class test_text_exception : public testing::Test
     {
-        test_text_exception();
-        test_int_code_exception();
-        test_errno_code_exception();
-    }
+    protected:
+        const char *m_text = "gbhjkgbdjs";
+        std::string m_text_string = m_text;
+        text_exception m_exception = text_exception(m_text);
+    };
 
-    void test_text_exception()
+    TEST_F(test_text_exception, basic)
     {
-        const char *text = "gbhjkgbdjs";
-        TEST_TRACE(text);
+        ASSERT_NE(m_exception.what(), nullptr);
+        EXPECT_EQ(m_exception.what(), m_exception.what());
+        EXPECT_STREQ(m_exception.what(), m_text);
+    };
 
-        text_exception e0{text};
-        {
-            TEST_CONDITION(string{e0.what()} == string{text});
-        }
-
-        {
-            text_exception e0_temp{e0};
-            text_exception e{e0_temp};
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-
-        {
-            text_exception e0_temp{e0};
-            text_exception e{std::move(e0_temp)};
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-
-        {
-            string text_str_temp{text};
-            text_exception e{text_str_temp};
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-
-        {
-            string text_str_temp{text};
-            text_exception e{std::move(text_str_temp)};
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-
-        {
-            exception &e0_ref{e0};
-            text_exception e{e0_ref};
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-
-        {
-            text_exception e0_temp{e0};
-            text_exception e{};
-            e = e0_temp;
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-
-        {
-            text_exception e0_temp{e0};
-            text_exception e{};
-            e = std::move(e0_temp);
-            TEST_CONDITION(string{e.what()} == string{text});
-        }
-    }
-
-    void test_int_code_exception()
+    TEST_F(test_text_exception, contruct_void)
     {
-        int code = 10086;
-        TEST_TRACE(code);
+        text_exception current_exception;
 
-        const int_code_exception e0{code};
-        {
-            TEST_CONDITION(string{e0.what()} == to_string(code));
-        }
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+    };
 
-        {
-            int_code_exception e0_temp{e0};
-            int_code_exception e{e0_temp};
-            TEST_CONDITION(string{e.what()} == to_string(code));
-        }
-
-        {
-            int_code_exception e0_temp{e0};
-            int_code_exception e{std::move(e0_temp)};
-            TEST_CONDITION(string{e.what()} == to_string(code));
-        }
-
-        {
-            int code_temp = code;
-            int_code_exception e{code_temp};
-            TEST_CONDITION(string{e.what()} == to_string(code));
-        }
-
-        {
-            int code_temp = code;
-            int_code_exception e{std::move(code_temp)};
-            TEST_CONDITION(string{e.what()} == to_string(code));
-        }
-
-        {
-            int_code_exception e0_temp{e0};
-            int_code_exception e{};
-            e = e0;
-            TEST_CONDITION(string{e.what()} == to_string(code));
-        }
-
-        {
-            int_code_exception e0_temp{e0};
-            int_code_exception e{};
-            e = std::move(e0);
-            TEST_CONDITION(string{e.what()} == to_string(code));
-        }
-    }
-
-    void test_errno_code_exception()
+    TEST_F(test_text_exception, contruct_copy)
     {
-        int code = E2BIG;
-        TEST_TRACE(code);
-        TEST_TRACE(strerror(code));
+        text_exception current_exception(m_exception);
 
-        const errno_code_exception e0{code};
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
 
-        {
-            errno_code_exception e0_temp{e0};
-            errno_code_exception e{e0_temp};
-            TEST_CONDITION(string{e.what()} == e0.what());
-        }
+    TEST_F(test_text_exception, contruct_move)
+    {
+        text_exception current_exception(std::move(m_exception));
 
-        {
-            errno_code_exception e0_temp{e0};
-            errno_code_exception e{std::move(e0_temp)};
-            TEST_CONDITION(string{e.what()} == e0.what());
-        }
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
 
-        {
-            int code_temp = code;
-            errno_code_exception e{code_temp};
-            TEST_CONDITION(string{e.what()} == e0.what());
-        }
+    TEST_F(test_text_exception, contruct_string)
+    {
+        text_exception current_exception(m_text_string);
 
-        {
-            int code_temp = code;
-            errno_code_exception e{std::move(code_temp)};
-            TEST_CONDITION(string{e.what()} == e0.what());
-        }
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
 
-        {
-            errno_code_exception e0_temp{e0};
-            errno_code_exception e{};
-            e = e0;
-            TEST_CONDITION(string{e.what()} == e0.what());
-        }
+    TEST_F(test_text_exception, contruct_move_string)
+    {
+        text_exception current_exception(std::move(m_text_string));
 
-        {
-            errno_code_exception e0_temp{e0};
-            errno_code_exception e{};
-            e = std::move(e0);
-            TEST_CONDITION(string{e.what()} == e0.what());
-        }
-    }
-};
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
 
-static test_exception s_test;
+    TEST_F(test_text_exception, contruct_exception)
+    {
+        const std::exception &m_exception_reference = m_exception;
+        text_exception current_exception(m_exception_reference);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
+
+    TEST_F(test_text_exception, assign_copy)
+    {
+        text_exception current_exception;
+        current_exception = m_exception;
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
+
+    TEST_F(test_text_exception, assign_move)
+    {
+        text_exception current_exception;
+        current_exception = std::move(m_exception);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), m_text);
+    };
+} // namespace
+
+// test_int_code_exception
+namespace
+{
+    class test_int_code_exception : public testing::Test
+    {
+    protected:
+        int m_code = 10086;
+        const int_code_exception m_exception = int_code_exception(m_code);
+    };
+
+    TEST_F(test_int_code_exception, basic)
+    {
+        ASSERT_NE(m_exception.what(), nullptr);
+        EXPECT_EQ(m_exception.what(), m_exception.what());
+        EXPECT_STREQ(m_exception.what(), std::to_string(m_code).c_str());
+    };
+
+    TEST_F(test_int_code_exception, contruct_void)
+    {
+        int_code_exception current_exception;
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+    };
+
+    TEST_F(test_int_code_exception, contruct_copy)
+    {
+        int_code_exception current_exception(m_exception);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), std::to_string(m_code).c_str());
+    };
+
+    TEST_F(test_int_code_exception, contruct_move)
+    {
+        int_code_exception current_exception(std::move(m_exception));
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), std::to_string(m_code).c_str());
+    };
+
+    TEST_F(test_int_code_exception, contruct_int)
+    {
+        int_code_exception current_exception(m_code);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), std::to_string(m_code).c_str());
+    };
+
+    TEST_F(test_int_code_exception, contruct_move_int)
+    {
+        int_code_exception current_exception(std::move(m_code));
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), std::to_string(m_code).c_str());
+    };
+
+    TEST_F(test_int_code_exception, assign_copy)
+    {
+        int_code_exception current_exception;
+        current_exception = m_exception;
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), std::to_string(m_code).c_str());
+    };
+
+    TEST_F(test_int_code_exception, assign_move)
+    {
+        int_code_exception current_exception;
+        current_exception = std::move(m_exception);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), std::to_string(m_code).c_str());
+    };
+} // namespace
+
+// test_errno_code_exception
+namespace
+{
+    class test_errno_code_exception : public testing::Test
+    {
+    protected:
+        int m_code = E2BIG;
+        const errno_code_exception m_exception = errno_code_exception(m_code);
+    };
+
+    TEST_F(test_errno_code_exception, basic)
+    {
+        ASSERT_NE(m_exception.what(), nullptr);
+        EXPECT_EQ(m_exception.what(), m_exception.what());
+        EXPECT_STREQ(m_exception.what(), strerror(m_code));
+    };
+
+    TEST_F(test_errno_code_exception, contruct_void)
+    {
+        errno_code_exception current_exception;
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+    };
+
+    TEST_F(test_errno_code_exception, contruct_copy)
+    {
+        errno_code_exception current_exception(m_exception);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), strerror(m_code));
+    };
+
+    TEST_F(test_errno_code_exception, contruct_move)
+    {
+        errno_code_exception current_exception(std::move(m_exception));
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), strerror(m_code));
+    };
+
+    TEST_F(test_errno_code_exception, contruct_int)
+    {
+        errno_code_exception current_exception(m_code);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), strerror(m_code));
+    };
+
+    TEST_F(test_errno_code_exception, contruct_move_int)
+    {
+        errno_code_exception current_exception(std::move(m_code));
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), strerror(m_code));
+    };
+
+    TEST_F(test_errno_code_exception, assign_copy)
+    {
+        errno_code_exception current_exception;
+        current_exception = m_exception;
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), strerror(m_code));
+    };
+
+    TEST_F(test_errno_code_exception, assign_move)
+    {
+        errno_code_exception current_exception;
+        current_exception = std::move(m_exception);
+
+        ASSERT_NE(current_exception.what(), nullptr);
+        EXPECT_EQ(current_exception.what(), current_exception.what());
+        EXPECT_STREQ(current_exception.what(), strerror(m_code));
+    };
+} // namespace
