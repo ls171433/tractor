dd8aefad5b05e62bf1abcc86424d7f59b326eb4e
Shuang Liang <shuangliang@microsoft.com>
2022-09-01 13:18:38 +0800

    fix endian fail

--- include/data/endian.hpp
+++ include/data/endian.hpp
@@ -3,6 +3,22 @@
 #include "general/compiler.hpp"
 #include "data/data.hpp"
 
+#define TRACTOR_IS_LITTLE_ENDIAN 0
+#define TRACTOR_IS_BIG_ENDIAN 0
+
+#if TRACTOR_IS_MSVC
+#undef TRACTOR_IS_LITTLE_ENDIAN
+#define TRACTOR_IS_LITTLE_ENDIAN 1
+#elif TRACTOR_IS_GCC || TRACTOR_IS_CLANG
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#undef TRACTOR_IS_LITTLE_ENDIAN
+#define TRACTOR_IS_LITTLE_ENDIAN 1
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#undef TRACTOR_IS_BIG_ENDIAN
+#define TRACTOR_IS_BIG_ENDIAN 1
+#endif
+#endif
+
 // type endian
 namespace tractor
 {
@@ -10,16 +26,10 @@ namespace tractor
     {
         little,
         big,
-#if is_msvc_v
+#if TRACTOR_IS_LITTLE_ENDIAN
         native = little,
-#elif is_gcc_v || is_clang_v
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        native = little,
-#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#elif TRACTOR_IS_BIG_ENDIAN
         native = big,
-#else
-        native,
-#endif
 #else
         native,
 #endif
--- include/general/compiler.hpp
+++ include/general/compiler.hpp
@@ -1,5 +1,20 @@
 #pragma once
 
+#define TRACTOR_IS_MSVC 0
+#define TRACTOR_IS_GCC 0
+#define TRACTOR_IS_CLANG 0
+
+#if defined _MSC_VER
+#undef TRACTOR_IS_MSVC
+#define TRACTOR_IS_MSVC 1
+#elif defined __GNUC__ && !defined __clang__
+#undef TRACTOR_IS_GCC
+#define TRACTOR_IS_GCC 1
+#elif defined __GNUC__ && defined __clang__
+#undef TRACTOR_IS_CLANG
+#define TRACTOR_IS_CLANG 1
+#endif
+
 namespace tractor
 {
     enum class compiler
@@ -7,17 +22,16 @@ namespace tractor
         msvc,
         gcc,
         clang,
-#if defined _MSC_VER
+#if TRACTOR_IS_MSVC
         native = msvc,
-#elif defined __GNUC__ && !defined __clang__
+#elif TRACTOR_IS_GCC
         native = gcc,
-#elif defined __GNUC__ && defined __clang__
+#elif TRACTOR_IS_CLANG
         native = clang,
 #else
         native,
 #endif
     };
-
 
     constexpr bool is_msvc_v = compiler::native == compiler::msvc;
     constexpr bool is_gcc_v = compiler::native == compiler::gcc;
