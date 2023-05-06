846f79d0ac1b991a3c765e127a54a2b5c541d379
Shuang Liang <shuangliang@microsoft.com>
2023-05-06 11:28:12 +0800

    use c++17 and new types

--- include/math/random.hpp
+++ include/math/random.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
-#include "type/basic_types.hpp"
+#include "type/types.hpp"
+#include "type/type_utilities.hpp"
 
 #include <random>
 #include <type_traits>
 
 namespace tractor
 {
-    template <class int_type_template, class enable = typename std::enable_if<std::is_integral<int_type_template>::value>::type>
+    template <class int_type_template, class enable = typename std::enable_if_t<std::is_integral_v<int_type_template>>>
     class int_random_base final
     {
     public:
         using int_type = int_type_template;
 
     public:
-        int_random_base() : int_random_base(min<int_type>(), max<int_type>()) {}
+        int_random_base() : int_random_base(min_value<int_type>, max_value<int_type>) {}
 
         int_random_base(int_type min, int_type max) : m_engine(std::random_device()()), m_distribution(min, max) {}
 
